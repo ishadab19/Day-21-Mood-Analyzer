efa8c05d1a4d4f4074f099c70f6f3da0fd3c1e0c
shadab <me.shadab19@gmail.com>
2022-11-01 15:11:16 +0530

    [Added - To Analyse message and return Happy]

--- Day-21-Exception/src/com/moodAnalyzer/java/MoodAnalyzerTest.java
+++ Day-21-Exception/src/com/moodAnalyzer/java/MoodAnalyzerTest.java
@@ -2,23 +2,29 @@ package com.moodAnalyzer.java;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-
 /**
- * To Check the Mood of the Person by String Entered. Emphasis on - Junit Test
- * Cases - Exception Handling - Custom Exceptions Author
+ * To Check the Mood of the Person by String Entered.
+ * Emphasis on - Junit Test Cases - Exception Handling - Custom Exceptions
+ * Author 
  *
- * Testing Class of MoodAnalyzerTest Created Object of Mood Analyzer Created a
- * Test Function and Imported Assertions and checking SAD or HAPPY
+ * Testing Class of MoodAnalyzerTest
+ * Created Object of Mood Analyzer
+ * Created a Test Function and Imported Assertions and checking SAD or HAPPY
  */
 
 public class MoodAnalyzerTest {
 	MoodAnalyzer moodAnalyze = new MoodAnalyzer();
 
-	@Test
-	public void if_MessageWasSad_ShouldReturnSad() {
-
-		String message = "I am in Sad Mood";
-		String result = moodAnalyze.moodCheck(message);
-		Assertions.assertEquals("SAD", result);
+	 @Test
+	    public void if_MessageWasSad_ShouldReturnSad(){
+	        String message = "I am in Sad Mood";
+	        String result1 = moodAnalyze.moodCheck(message);
+	        Assertions.assertEquals("SAD",result1);
 	}
+	 @Test
+	    public void if_MessageWasHappy_ShouldReturnHappy() {
+		 String message = "I am in Happy Mood";
+		 String result2 = moodAnalyze.moodCheck(message);
+		 Assertions.assertEquals("HAPPY", result2);
+	 }
 }
