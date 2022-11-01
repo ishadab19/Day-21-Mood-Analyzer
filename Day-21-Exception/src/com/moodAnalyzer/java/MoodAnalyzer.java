3e02e0d4e7473d18d70b5d664798ebeeda13c446
shadab <me.shadab19@gmail.com>
2022-11-01 17:13:30 +0530

    [Added - To handle exception if input is null]

--- Day-21-Exception/src/com/moodAnalyzer/java/MoodAnalyzer.java
+++ Day-21-Exception/src/com/moodAnalyzer/java/MoodAnalyzer.java
@@ -1,4 +1,5 @@
 package com.moodAnalyzer.java;
+
 /**
  * To Check the Mood of the Person by String Entered. Emphasis on - Junit Test
  * Cases - Exception Handling - Custom Exceptions
@@ -8,20 +9,27 @@ package com.moodAnalyzer.java;
 //Mood Analyzer Class
 public class MoodAnalyzer {
 	String message;
-	
+	 //Constructor created to pass message as mentioned in UC.
 	public MoodAnalyzer(String message) {
 		this.message = message;
 	}
 
-	 //Function to Check Mood of a String Entered
-    public String moodCheck(){
-        if(message.contains("Sad")) {
-            System.out.println("In SAD Mood");
-            return "SAD";
-        }
-        else{
-            System.out.println("In HAPPY Mood");
-            return "HAPPY";
-        }
+	// Function to Check Mood of a String Entered
+	public String moodCheck() {
+		// Created Try and Catch Blocks to Handle Exceptions if Occurs.
+		try {
+			if (message.contains("Sad")) {
+				System.out.println("In SAD Mood");
+				return "SAD";
+			} else {
+				System.out.println("In HAPPY Mood");
+				return "HAPPY";
+			}
+		}
+		// Don't Know what would be exception, So Took a Normal Exception to Catch,
+		// Whatever Exception Occurs.
+		catch (Exception exception) {
+			return "HaPPY";
+		}
 	}
 }
